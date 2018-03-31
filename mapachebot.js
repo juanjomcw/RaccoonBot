fb2d75761dec2c7f7afdc317892ebec233c9e854
juanjomcw <37007743+juanjomcw@users.noreply.github.com>
2018-03-30 17:43:58 -0700

    Update mapachebot.js

--- mapachebot.js
+++ mapachebot.js
@@ -69,35 +69,22 @@ client.on("message", (message) => {
    }else 
    if(message.content.startsWith(prefix + 'user')){
        let userm = message.mentions.users.first()
-
-if(!userm){
-  var user = message.author;
-      
-  const embed = new Discord.RichEmbed()
-    .setThumbnail(user.avatarURL)
-    .setAuthor(user.username+'#'+user.discriminator, user.avatarURL)
-    .addField('Jugando a', user.presence.game != null ? user.presence.game.name : "Nada", true)
-    .addField('ID', user.id, true)
-    .addField('Estado', user.presence.status, true)
-    .addField('Apodo', message.member.nickname, true)
-    .addField('Fecha de Ingreso', message.member.joinedAt.toDateString())
-    .addField('Roles', message.member.roles.map(roles => `\`${roles.name}\``).join(', '))
-    .setColor(10371071)
-        
-  message.channel.send({ embed });
-
-}else{
-  const embed = new Discord.RichEmbed()
-    .setThumbnail(userm.avatarURL)
-    .setAuthor(userm.username+'#'+userm.discriminator, userm.avatarURL)
-    .addField('Jugando a', userm.presence.game != null ? userm.presence.game.name : "Nada", true)
-    .addField('ID', userm.id, true)
-    .addField('Estado', userm.presence.status, true)
-    .setColor(10371071)
-    
-  message.channel.send({ embed });
-
-}else{
+       if(!userm){
+         var user = message.author;
+         
+           const embed = new Discord.RichEmbed()
+           .setThumbnail(user.avatarURL)
+           .setAuthor(user.username+'#'+user.discriminator, user.avatarURL)
+           .addField('Jugando a', user.presence.game != null ? user.presence.game.name : "Nada", true)
+           .addField('ID', user.id, true)
+           .addField('Estado', user.presence.status, true)
+           .addField('Apodo', message.member.nickname, true)
+           .addField('Fecha de Ingreso', message.member.joinedAt.toDateString())
+           .addField('Roles', message.member.roles.map(roles => `\`${roles.name}\``).join(', '))
+           .setColor(10371071)
+           
+          message.channel.send({ embed });
+       }else{
          const embed = new Discord.RichEmbed()
          .setThumbnail(userm.avatarURL)
          .setAuthor(userm.username+'#'+userm.discriminator, userm.avatarURL)
